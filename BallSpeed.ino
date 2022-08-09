cb3cca7ea10d8cf5e9a3cb6f7f757b524cecbf0d
cedarlakeinstruments <lyndon@cedarlakeinstruments.com>
2022-08-09 11:55:15 -0500

    Added audio
    Interfaced DF Robot MP3 player over software serial.

--- BallSpeed.ino
+++ BallSpeed.ino
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Project sponsor: Mutea Sabir
 // Email: mutea_sabir@hotmail.com
 // Creator: Cedar Lake Instruments LLC
@@ -37,14 +38,15 @@
 >  Of course, we can control the length of time or pulse as desired
  */
  
-#include <pt.h>
+#include <DFRobot_DF1201S.h>
+#include <SoftwareSerial.h>
 
 // I/O Connections:
 // Arduino pin   - I/O description 
 // Pin 2 - 7: ball sensor input
 // Pin 8: ball tray empty
 // Pin 9: LED output (needs drive transistor)
-// Pin xxx - DF Robot triggers
+// Pin 10,11 - DF Robot triggers
 
 #define LED_PIN LED_BUILTIN
 
@@ -57,12 +59,13 @@
 #define LAST_BALL 8
 
 #define BUF_SIZE 40
-static struct pt _pt1;
+//static struct pt _pt1;
 byte _pulseCount = 0;
 byte _threadPulseCount = 0;
 
 char buffer[50];
-
+SoftwareSerial DF1201SSerial(10, 11);  //RX  TX
+DFRobot_DF1201S DF1201S;
 
 //***************************************** USER SETUP *****************************
 // Number of LED flashes
@@ -157,18 +160,63 @@ void setup()
     // Initialize Serial port
     Serial.begin(115200);
 
+    // Pin configuration
     pinMode(LAST_BALL, INPUT_PULLUP);
     
     // Configure pins D2-D7 as inputs
     DDRD = 0;
 
     // Enable internal pullups
-    PORTD = BALL_MASK;
-    
-	  //PT_INIT(&_pt1);
-   Serial.println("BallSpeed 1.0 ready");
-}
+    PORTD = BALL_MASK;   
 
+    // Setup DFr
+    Serial.println("Start Audio Player setup");
+    DF1201SSerial.begin(115200);
+    bool df1201Ready = false;
+    for (int i=0; i < 5; i++)
+    {   if (DF1201S.begin(DF1201SSerial))
+        {
+            df1201Ready = true;
+            break;
+        }
+        Serial.println("DF1201 failed, please check the wire connection!");
+        delay(1000);
+    }
+    
+    if (df1201Ready)
+    {
+        Serial.println("DF1201 connected");
+    }
+    
+    delay(100);
+    /*Set volume to 20*/
+    DF1201S.setVol(20);
+    delay(100);
+    
+    /*Enter music mode*/
+    DF1201S.switchFunction(DF1201S.MUSIC);
+    // Wait for prompt to stop
+    delay(2000);
+    
+    //Turn off the prompt tone (Power-down save) 
+    DF1201S.setPrompt(false);
+    delay(100);
+    
+    DF1201S.setPlayMode(DF1201S.SINGLE);
+    delay(100);
+    
+    //Enable amplifier chip 
+    DF1201S.enableAMP();
+    delay(100);
+  
+    Serial.print("The number of files available to play:");
+    //The number of files available to play
+    Serial.println(DF1201S.getTotalFile());
+  
+    Serial.println("DF1201 configured");
+    Serial.println("BallSpeed 1.0 ready");
+}
+    
 void loop() 
 {  
     const int FIRST_PIN = 2;
@@ -206,50 +254,45 @@ void celebrate(int pocket)
     sprintf(buffer,"Ball %d time: %d",pocket+1, _balls[pocket].transitionTime);
     Serial.println(buffer);
 
-    int flashes = flashCount(_balls[pocket].transitionTime);
-    for (int i = 0; i < flashes; i++)
+    const String files[] = {"/fast.mp3", "/medium.mp3", "/slow.mp3"};
+    const int flashes[] = {FLASH_COUNT_FAST, FLASH_COUNT_MEDIUM,FLASH_COUNT_SLOW};
+
+    int index = flashCountIndex(_balls[pocket].transitionTime);
+    if (index < 3)
     {
-        digitalWrite(LED_PIN, HIGH);
-        delay(BLINK_SPEED);
-        digitalWrite(LED_PIN, LOW);
-        delay(BLINK_SPEED);        
+        Serial.print("Playing ");Serial.println(files[index]);
+        DF1201S.playSpecFile(files[index]);        
+        
+        for (int i = 0; i < flashes[index]; i++)
+        {
+            digitalWrite(LED_PIN, HIGH);
+            delay(BLINK_SPEED);
+            digitalWrite(LED_PIN, LOW);
+            delay(BLINK_SPEED);        
+        }
+    }
+    else
+    {
+        Serial.println("Index out of range");
     }
 }
 
 // Returns number of flashes based on transition time
-int flashCount(int time)
+int flashCountIndex(int time)
 {
     if (time <= FAST_MS)
     {
         Serial.println("Fast");
-        return FLASH_COUNT_FAST;
+        return 0;
     }
     else if (time <= MEDIUM_MS)
     {
         Serial.println("Medium");
-        return FLASH_COUNT_MEDIUM;
+        return 1;
     }
     else
     {
         Serial.println("Slow");
-        return FLASH_COUNT_SLOW;
+        return 2;
     }
-}
-
-// Pulse relay on a separate thread
-static int relayPulse(struct pt *pt)
-{
-	static unsigned long timestamp = 0;
-	PT_BEGIN(pt);
-	while (_threadPulseCount > 0)
-	{
-		digitalWrite(LED_BUILTIN, HIGH);
-		timestamp = millis();
-		PT_WAIT_UNTIL(pt, millis() - timestamp > PULSE_ON_TIME);
-		digitalWrite(LED_BUILTIN, LOW);
-		timestamp = millis();
-		PT_WAIT_UNTIL(pt, millis() - timestamp > PULSE_OFF_TIME);
-		_threadPulseCount--;
-	}
-	PT_END(pt);
 }
